1a47ba4dc6558bafedb87f5d2a89c685523a3a2b
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-01-30 15:14:14 -0700

    Initial Commit

---
+++ stroustrup-programming-and-principles-work/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stroustrup-programming-and-principles-work
+//
+//  Created by Drew Giambattista on 1/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
